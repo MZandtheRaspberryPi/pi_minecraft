35c458df4e6181d8760fc27de6a9697b9d05a24f
MZandtheRaspberryPi <46413365+MZandtheRaspberryPi@users.noreply.github.com>
2021-12-06 06:07:30 +0100

    Create check_ip.sh

---
+++ check_ip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file_path="/home/mikey/Documents/ip_address.txt"
+
+password=$1
+ip_address=$(wget -qO- http://ipecho.net/plain)
+
+touch $file_path
+
+old_ip_address=$(cat $file_path)
+
+if [[ $old_ip_address == $ip_address ]]; then
+  exit 0
+else
+  echo "$ip_address" >>$file_path
+
+  curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
+    --mail-from 'camminecraftserver@gmail.com' \
+    --mail-rcpt 'ziegltrum@gmail.com' \
+    --user "camminecraftserver@gmail.com:$password" \
+    -T <(echo -e "From: camminecraftserver@gmail.com\nTo: ziegltrum@gmail.com\nSubject: MC IP: $ip_address\n\nHello,\nThe ip address for Cam's minecraft serv$
+fi
